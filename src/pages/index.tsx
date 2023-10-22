762327a64ff2e97c38b08fa50da7a996fc83503b
Dario de souza <darioo_dario@hotmail.com>
2023-10-22 12:24:38 -0300

    styles: footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import styles from '@/styles/home.module.scss'
+import Image from 'next/image'
+
+import techsImage from '@/../public/images/techs.svg'
 
 export default function Home() {
   return (
@@ -46,6 +49,16 @@ export default function Home() {
             <span>Criar sistemas web, sites usando as tecnologias mais modernas e requisitadas pelo mercado.</span>
           </section>
         </div>
+
+        <div className={styles.nextLevelContent}>
+          <Image quality={100} src={techsImage} alt="Tecnologias" />
+          <h2>Mais de <span className={styles.alunos}>15 mil</span> já levaram sua carreira ao próximo nivel.</h2>
+          <span>E você vai perder a chance de evoluir de uma vez por todas?</span>
+          <a>
+            <button>ACESSAR TURMA!</button>
+          </a>
+        </div>
+
 
 
       </main>
