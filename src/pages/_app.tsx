72348d6037f6eb51fb919f1712aeaa10ce14289b
Dario de souza <darioo_dario@hotmail.com>
2023-10-22 10:20:30 -0300

    fix: eslint config error semi

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import type { AppProps } from 'next/app';
-import { Poppins } from 'next/font/google';
-import '@/styles/global.scss';
-import Header from '@/components/Header';
+import type { AppProps } from 'next/app'
+import { Poppins } from 'next/font/google'
+import '@/styles/global.scss'
+import Header from '@/components/Header'
 
 const popins = Poppins({
   weight: ['400', '500', '700', '900'],
   subsets: ['latin'],
-});
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -14,5 +14,5 @@ export default function App({ Component, pageProps }: AppProps) {
       <Header />
       <Component {...pageProps} />;
     </div>
-  );
+  )
 }
