7e99cef0a03d5c1f9de42f28908577985ccde5ef
Dario de souza <darioo_dario@hotmail.com>
2023-10-30 21:26:33 -0300

    prismic com react versão antiga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,7 @@ import type { AppProps } from 'next/app'
 import { Poppins } from 'next/font/google'
 import '@/styles/global.scss'
 import Header from '@/components/Header'
-import { PrismicProvider } from '@prismicio/react'
-import { client } from '@/prismic'
+
 
 const popins = Poppins({
   weight: ['400', '500', '700', '900'],
@@ -12,11 +11,11 @@ const popins = Poppins({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PrismicProvider client={client}>
-      <div className={popins.className}>
-        <Header />
-        <Component {...pageProps} />
-      </div>
-    </PrismicProvider>
+
+    <div className={popins.className}>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,45 @@
+import { GetStaticProps } from 'next'
 
 import Head from 'next/head'
-import styles from '@/styles/home.module.scss'
+import styles from '../styles/home.module.scss'
+
 import Image from 'next/image'
-import techsImage from '@/../public/images/techs.svg'
-import { useFirstPrismicDocument } from '@prismicio/react'
-import dataPrismic, { Content } from '@/utils/prismic_data'
+import techsImage from '../../public/images/techs.svg'
 
+import { getPrismicClient } from '../services/prismic'
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 
+type Content = {
+  title: string;
+  titleContent: string;
+  linkAction: string;
+  mobileTitle: string;
+  mobileContent: string;
+  mobileBanner: string;
+  webTitle: string;
+  webContent: string;
+  webBanner: string;
+}
 
+interface ContentProps {
+  content: Content;
+}
 
-
-
-
-
-export default function Home() {
-  const [document] = useFirstPrismicDocument()
-  let content: Content = {} as Content
-  if (document) {
-    content = dataPrismic(document)
-  }
+export default function Home({ content }: ContentProps) {
 
   return (
     <>
       <Head>
-        <title>Sujeito | Home</title>
+        <title>Apaixonado por tecnologia - Sujeito Programador</title>
       </Head>
       <main className={styles.container}>
         <div className={styles.containerHeader}>
           <section className={styles.ctaText}>
             <h1>{content.title}</h1>
-            <span>asdfwert4</span>
-            <a href="">
+            <span>{content.titleContent}</span>
+            <a href={content.linkAction}>
               <button>
-
                 COMEÇAR AGORA!
               </button>
             </a>
@@ -48,21 +55,21 @@ export default function Home() {
 
         <div className={styles.sectionContent}>
           <section>
-            <h2>{content.title}</h2>
-            <span>faswertwertwerwer</span>
+            <h2>{content.mobileTitle}</h2>
+            <span>{content.mobileContent}</span>
           </section>
 
-          <img src="" alt="Conteúdos desenvolvimento de apps" />
+          <img src={content.mobileBanner} alt="Conteúdos desenvolvimento de apps" />
         </div>
 
         <hr className={styles.divisor} />
 
         <div className={styles.sectionContent}>
-          <img src="" alt="Conteúdos desenvolvimento de aplicacoes web" />
+          <img src={content.webBanner} alt="Conteúdos desenvolvimento de aplicacoes web" />
 
           <section>
-            <h2>gwesdrtwertwertewrt</h2>
-            <span>sedfserwer</span>
+            <h2>{content.webTitle}</h2>
+            <span>{content.webContent}</span>
           </section>
         </div>
 
@@ -70,41 +77,50 @@ export default function Home() {
           <Image quality={100} src={techsImage} alt="Tecnologias" />
           <h2>Mais de <span className={styles.alunos}>15 mil</span> já levaram sua carreira ao próximo nivel.</h2>
           <span>E você vai perder a chance de evoluir de uma vez por todas?</span>
-          <a href="">
+          <a href={content.linkAction}>
             <button>ACESSAR TURMA!</button>
           </a>
         </div>
+
+
       </main>
     </>
   )
 }
 
-
-// export const getStaticProps: GetStaticProps = async () => {
-
-//   const {
-//     title, sub_title, link_action,
-//     mobile, mobile_content, mobile_banner,
-//     title_web, web_content, web_banner
-//   } = response.results[0].data
-
-//   const content = {
-//     title: RichText.asText(title),
-//     titleContent: RichText.asText(sub_title),
-//     linkAction: link_action.url,
-//     mobileTitle: RichText.asText(mobile),
-//     mobileContent: RichText.asText(mobile_content),
-//     mobileBanner: mobile_banner.url,
-//     webTitle: RichText.asText(title_web),
-//     webContent: RichText.asText(web_content),
-//     webBanner: web_banner.url
-//   }
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
 
 
-//   return {
-//     props: {
+  const response = await prismic.query([
+    Prismic.Predicates.at('document.type', 'home')
+  ])
 
-//     },
+  // console.log(response.results[0].data);
 
-//   }
-// }
+  const {
+    title, sub_title, link_action,
+    mobile, mobile_content, mobile_banner,
+    title_web, web_content, web_banner
+  } = response.results[0].data
+
+  const content = {
+    title: RichText.asText(title),
+    titleContent: RichText.asText(sub_title),
+    linkAction: link_action.url,
+    mobileTitle: RichText.asText(mobile),
+    mobileContent: RichText.asText(mobile_content),
+    mobileBanner: mobile_banner.url,
+    webTitle: RichText.asText(title_web),
+    webContent: RichText.asText(web_content),
+    webBanner: web_banner.url
+  }
+
+
+  return {
+    props: {
+      content
+    },
+    revalidate: 60 * 2 // A cada 2 minutos
+  }
+}
