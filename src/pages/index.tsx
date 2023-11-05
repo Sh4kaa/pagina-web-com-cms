b16b913364adcc2447dd37985369fad81ad1fb36
Dario de souza <darioo_dario@hotmail.com>
2023-11-05 12:33:14 -0300

    fix: renderização de dados do prismic para texto cru

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 
 import Head from 'next/head'
 import styles from '../styles/home.module.scss'
-import Image from 'next/image'
 import techsImage from '../../public/images/techs.svg'
 import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
 import { createClient } from '@/prismicio'
-import { asHTML } from '@prismicio/helpers'
+import { asText } from '@prismicio/helpers'
 import { asLink } from '@prismicio/client'
+import Image from 'next/image'
 
 
 type PageProps = InferGetStaticPropsType<typeof getStaticProps>
@@ -45,51 +45,49 @@ export default function Home({ content }: PageProps) {
             <span>
               {content.titleContent}
             </span>
-            <a href={content.linkAction}>
-              <button>
-                COMEÇAR AGORA!
-              </button>
-            </a>
+            {content.linkAction && (
+              <a href={content.linkAction}>
+                <button>
+                  COMEÇAR AGORA!
+                </button>
+              </a>
+            )}
           </section>
-
           <img
             src="/images/banner-conteudos.png"
             alt="Conteúdos Sujeito Programador"
           />
         </div>
-
         <hr className={styles.divisor} />
-
         <div className={styles.sectionContent}>
           <section>
             <h2>{content.mobileTitle}</h2>
             <span>{content.mobileContent}</span>
           </section>
-
-          <img src={content.mobileBanner} alt="Conteúdos desenvolvimento de apps" />
+          {content.mobileBanner && (
+            <img src={content.mobileBanner} alt="Conteúdos desenvolvimento de apps" />
+          )}
         </div>
-
         <hr className={styles.divisor} />
-
         <div className={styles.sectionContent}>
-          <img src={content.webBanner} alt="Conteúdos desenvolvimento de aplicacoes web" />
-
+          {content.webBanner && (
+            <img src={content.webBanner} alt="Conteúdos desenvolvimento de aplicacoes web" />
+          )}
           <section>
             <h2>{content.webTitle}</h2>
             <span>{content.webContent}</span>
           </section>
         </div>
-
         <div className={styles.nextLevelContent}>
-          <Image quality={100} src={techsImage} alt="Tecnologias" />
+          <Image src={techsImage} alt="Tecnologias" quality={100} />
           <h2>Mais de <span className={styles.alunos}>15 mil</span> já levaram sua carreira ao próximo nivel.</h2>
           <span>E você vai perder a chance de evoluir de uma vez por todas?</span>
-          <a href={content.linkAction}>
-            <button>ACESSAR TURMA!</button>
-          </a>
+          {content.linkAction && (
+            <a href={content.linkAction}>
+              <button>ACESSAR TURMA!</button>
+            </a>
+          )}
         </div>
-
-
       </main>
     </>
   )
@@ -106,14 +104,14 @@ export async function getStaticProps({
 
 
   const content = {
-    title: page.data.title[0]?.text,
-    titleContent: asHTML(page.data.sub_title),
+    title: asText(page.data.title),
+    titleContent: asText(page.data.sub_title),
     linkAction: asLink(page.data.link_action),
-    mobileTitle: asHTML(page.data.mobile),
-    mobileContent: asHTML(page.data.mobile_content),
-    mobileBanner: page.data.mobile_banner.url,
-    webTitle: asHTML(page.data.title_web),
-    webContent: asHTML(page.data.web_content),
+    mobileTitle: asText(page.data.mobile),
+    mobileContent: asText(page.data.mobile_content),
+    mobileBanner: page.data.mobile_banner?.url,
+    webTitle: asText(page.data.title_web),
+    webContent: asText(page.data.web_content),
     webBanner: page.data.web_banner.url,
   }
 
